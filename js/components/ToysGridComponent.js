c8f3949b70fbfa4d098453d674a28d4a9a176fca
Gerardas Ciegis <gerardasc@gmail.com>
2021-11-14 14:10:22 +0200

    4 Pasiruosta kompnentu kurimui

---
+++ js/components/ToysGridComponent.js
@@ -0,0 +1,12 @@
+class ToysGridComponent {
+  constructor() {
+    
+    this.init();
+  }
+
+  init = () => {
+    this.htmlElement = document.createElement('div');
+    this.htmlElement.innerHTML = 'Komponentas';
+  }
+
+}
