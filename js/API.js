334377eff84922ebc07b29cfd575ddea784ca206
Gerardas Ciegis <gerardasc@gmail.com>
2021-11-14 14:32:26 +0200

    5: HTTP requestai

---
+++ js/API.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3000'
+
+class API {
+  static fetchToys = (success, failure) => {
+    fetch(`${baseURL}/toys`)
+    .then(res => res.json())
+    .then(success)
+    .catch(failure)
+  }
+
+  static deleteToy = (id, success, failure) => {
+    fetch(`${baseURL}/toys/${id}`, { method: 'DELETE' })
+    .then(res => res.ok ? success() : failure(res.statusText))
+    .catch(failure)
+  }
+}
+
+API.fetchToys(
+  console.log,
+  console.log
+)
+
+API.deleteToy(
+  "1",
+  () => console.log('Istrinta'),
+  console.error
+)
