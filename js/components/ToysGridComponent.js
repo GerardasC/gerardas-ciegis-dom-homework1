e4f84d6feb2e805e3a6dc4e838dfbe9c54425c92
Gerardas Ciegis <gerardasc@gmail.com>
2021-11-14 15:10:51 +0200

    7: laukimo animacija, duomenu atvaizdavimas

--- js/components/ToysGridComponent.js
+++ js/components/ToysGridComponent.js
@@ -8,8 +8,13 @@ class ToysGridComponent {
   }
 
   
-  fetchToys = () => API.fetchToys(this.saveToys, alert);
+  fetchToys = () => setTimeout ( () => {
+    
+    API.fetchToys(this.saveToys, alert)
   
+  }, 1000)
+
+
   saveToys = (toys) => {
     this.state.toys = toys;
     this.state.loading = false;
@@ -20,16 +25,29 @@ class ToysGridComponent {
     this.state.loading = true;
     this.fetchToys();
     this.htmlElement = document.createElement('div');
+    this.htmlElement.className = 'row g-3';
     this.render();
+  }
+
+  wrapInColumn = (element) => {
+    const column = document.createElement('div');
+    column.className = 'col-12 col-sm-6 col-lg-4 col-xl-3';
+    column.appendChild(element);
+    return column;
   }
 
   render = () => {
     const {loading, toys} = this.state;
     if (loading) {
-      this.htmlElement.innerHTML = 'siunciama'
+      this.htmlElement.innerHTML = '<div class="text-center"><img src="assets/loading.gif"/></div>'
     } else {
-      console.log(toys)
-      this.htmlElement.innerHTML = 'parsiusta'
+      this.htmlElement.innerHTML = '';
+      const toyElements = toys
+        .map(x => new ToyCardComponent(x))
+        .map(x => x.htmlElement)
+        .map(this.wrapInColumn);
+
+        this.htmlElement.append(...toyElements);
     }
   }
 
